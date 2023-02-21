9a0c5d8c7dc94e5fe24d2e5e9693e86c88ab0151
misohu <michal.hucko@canonical.com>
2023-02-21 21:45:43 +0100

    KF-737 test AWS runner

--- .github/workflows/setup_environment.sh
+++ .github/workflows/setup_environment.sh
@@ -25,10 +25,3 @@ sudo snap refresh charmcraft --channel latest/candidate
 sudo usermod -a -G microk8s ubuntu
 sudo chown -f -R ubuntu /home/ubuntu/.kube
 newgrp microk8s
-microk8s enable dns storage rbac metallb:10.64.140.43-10.64.140.49
-microk8s kubectl -n kube-system rollout status deployment/hostpath-provisioner
-juju bootstrap --debug --verbose microk8s uk8s-controller --model-default test-mode=true --model-default automatically-retry-hooks=false --model-default logging-config="<root>=DEBUG" --agent-version=2.9.34 --bootstrap-constraints=""
-juju add-model testing
-sudo snap install kubectl --classic
-mkdir -p /home/ubuntu/.kube
-chown -f -R ubuntu /home/ubuntu/.kube
---
+++ .github/workflows/setup_microk8s.sh
@@ -0,0 +1,8 @@
+#!/bin.bash
+microk8s enable dns storage rbac metallb:10.64.140.43-10.64.140.49
+microk8s kubectl -n kube-system rollout status deployment/hostpath-provisioner
+juju bootstrap --debug --verbose microk8s uk8s-controller --model-default test-mode=true --model-default automatically-retry-hooks=false --model-default logging-config="<root>=DEBUG" --agent-version=2.9.34 --bootstrap-constraints=""
+juju add-model testing
+sudo snap install kubectl --classic
+mkdir -p /home/ubuntu/.kube
+chown -f -R ubuntu /home/ubuntu/.kube
