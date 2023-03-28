394e7f8f9131e31d554c17240395e300cf82d305
misohu <michal.hucko@canonical.com>
2023-03-28 09:05:34 +0200

    KF-739 wait after deployment

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -108,7 +108,7 @@ class TestCharm:
 
         await ops_test.model.wait_for_idle(
             apps=[CHARM_NAME],
-            status="active",
+            status="waiting",
             raise_on_blocked=False,
             raise_on_error=False,
             timeout=600,
@@ -213,7 +213,6 @@ class TestCharm:
             raise_on_blocked=False,
             raise_on_error=False,
             timeout=600,
-            idle_period=300,
         )
         await ops_test.model.relate(RESOURCE_DISPATCHER_CHARM_NAME, CHARM_NAME)
 
