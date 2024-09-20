bbb435a8526ba9fd7a94b962d8197faad78de245
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-09-20 16:41:56 +0200

    feat: add Terraform module for mlflow-server (#267)
    * feat: add Terraform module for mlflow-server
    
    This commit adds the terraform/ directory to the root of the repository to host
    the Terraform module of this charm. This follows the standard set in CC006.
    For more information please also refer to canonical/argo-operators/pull/198.
    
    Fixes #266

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+resource "juju_application" "mlflow_server" {
+  charm {
+    name     = "mlflow-server"
+    channel  = var.channel
+    revision = var.revision
+  }
+  config    = var.config
+  model     = var.model_name
+  name      = var.app_name
+  resources = var.resources
+  trust     = true
+  units     = 1
+}
