77768ca787660116eab9fd4b7e85b21931ca1cb7
afgambin <103958447+afgambin@users.noreply.github.com>
2025-10-14 13:40:00 +0100

    Update sitemap config

--- docs/conf.py
+++ docs/conf.py
@@ -179,14 +179,9 @@ slug = 'charmed-mlflow'
 
 html_baseurl = 'https://documentation.ubuntu.com/charmed-mlflow/'
 
-# URL scheme. Add language and version scheme elements.
-# When configured with RTD variables, check for RTD environment so manual runs succeed:
+# URL scheme. 
 
-if 'READTHEDOCS_VERSION' in os.environ:
-    version = os.environ["READTHEDOCS_VERSION"]
-    sitemap_url_scheme = '{version}{link}'
-else:
-    sitemap_url_scheme = 'MANUAL/{link}'
+sitemap_url_scheme = '{link}'
 
 # Include `lastmod` dates in the sitemap:
 
