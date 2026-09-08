f10a03639c97ec05e9f74527c672afc27da1e4da
MattiaSarti <nmattia1@gmail.com>
2026-09-08 17:21:29 +0200

    style: fix comment grammar (again)

--- tests/integration/test_charm_ambient.py
+++ tests/integration/test_charm_ambient.py
@@ -433,7 +433,7 @@ class TestCharm:
                 assert role["workspace"] == TEST_WORKSPACE
         # when the identity is a newly seen one:
         else:
-            # asserting the user MLflow has no grants:
+            # asserting the MLflow user has no grants:
             assert user_roles == []
 
         mlflow_subprocess.terminate()
