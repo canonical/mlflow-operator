43512652945e301966a37f65b63085ea5e68b5e5
MattiaSarti <nmattia1@gmail.com>
2026-09-11 16:19:10 +0200

    style: fix spelling

--- src/auth/custom_userid_header_auth.py
+++ src/auth/custom_userid_header_auth.py
@@ -33,7 +33,7 @@ from werkzeug.datastructures import Authorization
 HARDCODED_TEST_IDENTITY = "charm-test-user"
 HARDCODED_TEST_WORKSPACE = "default"
 
-# NOTE: there environment variables correspond to charm configs and are guaranteed to always be up
+# NOTE: these environment variables correspond to charm configs and are guaranteed to always be up
 # to date because the charm reloads the tracking server on config changes:
 _IDENTITY_HEADER_NAME = os.environ["IDENTITY_HEADER_NAME"]
 _RAW_IDENTITY_ALIASES = os.environ["IDENTITY_ALIASES"]
