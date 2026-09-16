605f32da18f86708b23af280b6c0a0a0eef67f70
MattiaSarti <nmattia1@gmail.com>
2026-09-16 09:11:20 +0200

    chore: switch from charm library to Python package for istio beacon

--- src/charm.py
+++ src/charm.py
@@ -12,16 +12,16 @@ from urllib.parse import urlparse
 
 import botocore.exceptions
 import yaml
+from canonical_service_mesh import (
+    MeshType,
+    PolicyResourceManager,
+    ServiceMeshConsumer,
+)
 from charmed_kubeflow_chisme.exceptions import ErrorWithStatus
 from charmed_kubeflow_chisme.pebble import update_layer
 from charmed_kubeflow_chisme.service_mesh import generate_allow_all_authorization_policy
 from charms.data_platform_libs.v0.data_interfaces import DatabaseRequires
 from charms.grafana_k8s.v0.grafana_dashboard import GrafanaDashboardProvider
-from charms.istio_beacon_k8s.v0.service_mesh import (
-    MeshType,
-    PolicyResourceManager,
-    ServiceMeshConsumer,
-)
 from charms.istio_ingress_k8s.v0.istio_ingress_route import (
     BackendRef,
     HTTPPathMatch,
