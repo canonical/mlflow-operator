2afe9ff7dd05e4acffe3687fe65d3974d9c26446
MattiaSarti <nmattia1@gmail.com>
2026-09-17 12:04:48 +0200

    chore: fix dependencies

--- src/charm.py
+++ src/charm.py
@@ -16,6 +16,7 @@ from canonical_service_mesh.k8s.resource_manager import PolicyResourceManager
 from charmed_kubeflow_chisme.exceptions import ErrorWithStatus
 from charmed_kubeflow_chisme.pebble import update_layer
 from charmed_kubeflow_chisme.service_mesh import generate_allow_all_authorization_policy
+from charmlibs.interfaces.service_mesh import MeshType, ServiceMeshConsumer
 from charms.data_platform_libs.v0.data_interfaces import DatabaseRequires
 from charms.grafana_k8s.v0.grafana_dashboard import GrafanaDashboardProvider
 from charms.istio_ingress_k8s.v0.istio_ingress_route import (
@@ -48,7 +49,6 @@ from dpcharmlibs.interfaces import (
     ResourceProviderEventHandler,
     ResourceProviderModel,
 )
-from dpcharmlibs.interfaces.service_mesh import MeshType, ServiceMeshConsumer
 from jinja2 import Template
 from lightkube import Client
 from lightkube.models.core_v1 import ServicePort
