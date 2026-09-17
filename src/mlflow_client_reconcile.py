04a30a00182e5150954e0588f5084b9743e51487
MattiaSarti <nmattia1@gmail.com>
2026-09-17 11:19:20 +0200

    style: make code maintainable

--- src/mlflow_client_reconcile.py
+++ src/mlflow_client_reconcile.py
@@ -139,7 +139,7 @@ usernames_by_id = {user.id: user.username for user in store.list_users()}
 for role in store.list_roles([SYSTEM_WORKSPACE]):
     if not role.name.startswith(SUPER_ADMIN_ROLE_PREFIX) or role.name in wanted_super_admin_roles:
         continue
-    username = usernames_by_id.get(int(role.name[len(SUPER_ADMIN_ROLE_PREFIX):]))
+    username = usernames_by_id.get(int(role.name[len(SUPER_ADMIN_ROLE_PREFIX) :]))
     if username and username != protected_admin:
         store.update_user(username, is_admin=False)
     store.delete_role(role.id)
